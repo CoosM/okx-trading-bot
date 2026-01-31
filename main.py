a0083f6005527e1153cef04f44e63d100082e23a
CoosM <valera77999@gmail.com>
2026-01-31 06:01:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os, time, hmac, hashlib, base64, json, requests
 
 app = Flask(__name__)
 
+TEST_MODE = True
+
 OKX_API_KEY = os.getenv("OKX_API_KEY")
 OKX_SECRET_KEY = os.getenv("OKX_SECRET_KEY")
 OKX_PASSPHRASE = os.getenv("OKX_PASSPHRASE")
@@ -22,6 +24,11 @@ def webhook():
     data = request.json
     print("Webhook received:", data)
 
+    if data.get("test") == "BUY" and TEST_MODE:
+        print("🟡 TEST MODE: BUY получен")
+        print("Данные:", data)
+        return {"status": "test_ok"}, 200
+        
     body = json.dumps(data)
     path = "/api/v5/trade/order"
     url = OKX_BASE_URL + path
