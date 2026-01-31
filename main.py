e34310fb1bdcce1d6c27febd064e00d956b40479
CoosM <valera77999@gmail.com>
2026-01-31 21:41:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -64,14 +64,13 @@ def buy_spot():
     r = requests.post(url, headers=headers, data=body_json).json()
 
     # считаем шаг
-    # считаем шаг
-if r.get("code") == "0" and r.get("data"):
-    order = r["data"][0]
+    if r.get("code") == "0" and r.get("data"):
+        order = r["data"][0]
 
-    filled = float(order.get("accFillSz", 0))
-    if filled > 0:
-        total_qty += filled
-        steps += 1
+        filled = float(order.get("accFillSz", 0))
+        if filled > 0:
+            total_qty += filled
+            steps += 1
 
     return r
 
